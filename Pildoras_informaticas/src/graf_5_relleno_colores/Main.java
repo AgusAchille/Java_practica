49ea240373c9fbc1e82c78cbad808f99b00c69ee
Agustín Achille <agustin.a.fr@gmail.com>
2019-01-09 14:46:55 -0300

    Relleno y colores

---
+++ Pildoras_informaticas/src/graf_5_relleno_colores/Main.java
@@ -0,0 +1,9 @@
+package graf_5_relleno_colores;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new Ventana();
+
+	}
+}
--- Pildoras_informaticas/src/graf_5_relleno_colores/Ventana.java
+++ Pildoras_informaticas/src/graf_5_relleno_colores/Ventana.java
@@ -1,4 +1,4 @@
-package graf_4_2D;
+package graf_5_relleno_colores;
 
 import javax.swing.*;
 import java.awt.*;
@@ -25,18 +25,23 @@ class Capa1 extends JPanel {
 		Graphics2D g2D = (Graphics2D) g;
 
 		Rectangle2D rectangulo = new Rectangle2D.Double(100, 100, 200, 150);
-		g2D.draw(rectangulo);
+		
+		g2D.setColor(new Color(50, 133, 191));
+		g2D.fill(rectangulo);
 
 		Ellipse2D circulo = new Ellipse2D.Double();
 
 		double radio = Math.hypot(200, 150) / 2;
 		circulo.setFrameFromCenter(200, 175, 200 + radio, 175 + radio);
-		g2D.draw(circulo);
+		
+		g2D.setColor(new Color(243,236,100));
+		
+		g2D.fill(circulo);
 
+		g2D.setColor(Color.WHITE);
 		g2D.draw(new Line2D.Double(100, 100, 300, 250));
 
 		
 		setBackground(new Color(41, 45, 62));
-		setForeground(new Color(199, 133, 191));
 	}
 }
