957004766dd2d3dce7fe8a8ee9975fd1439b80e6
Agustín Achille <agustin.a.fr@gmail.com>
2019-01-18 20:14:35 -0300

    Clase adaptadora de eventos, array de ventanas

---
+++ Pildoras_informaticas/src/g_events_4_clases_adaptadoras/Main.java
@@ -0,0 +1,12 @@
+package g_events_4_clases_adaptadoras;
+
+public class Main {
+	public static void main(String[] args ) {
+		Ventana[] ventanas = new Ventana[2];
+		
+		for (int i=0; i<ventanas.length;i++) {
+			ventanas[i] = new Ventana();
+		}
+		
+	}
+}
