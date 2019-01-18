d1490805a8cb13a9e9a232f658ec305bc9c07de9
Agustín Achille <agustin.a.fr@gmail.com>
2019-01-18 14:42:25 -0300

    Trabajando con 7 eventos de ventanas, en múltiples ventanas

---
+++ Pildoras_informaticas/src/g_events_3_eventos_ventanas/Main.java
@@ -0,0 +1,9 @@
+package g_events_3_eventos_ventanas;
+
+public class Main{
+	public static void main(String[] args) {
+		new Ventana();
+		new Ventana();
+		new Ventana();
+	}
+}
---
+++ Pildoras_informaticas/src/g_events_3_eventos_ventanas/Ventana.java
@@ -0,0 +1,67 @@
+package g_events_3_eventos_ventanas;
+
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Ventana extends JFrame {
+	private static int sig_num_ventana=1;
+	private int num_ventana;
+	
+	public Ventana() {
+		num_ventana = sig_num_ventana;
+		sig_num_ventana++;
+		setTitle("Ventana " + num_ventana);
+		setVisible(true);
+		
+		// el código extra es para que las ventanas aparezcan desplazadas
+		// según el numero de ventana
+		setBounds(100+50*num_ventana, 100+50*num_ventana, 400, 300);
+		
+		// Solo la primer ventana va a cerrar todas las ventanas cuando se cierre
+		if (num_ventana == 1)
+			setDefaultCloseOperation(EXIT_ON_CLOSE);
+		else
+			setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		
+		Ventana_listener vent_list = new Ventana_listener();
+		addWindowListener(vent_list);
+
+	}
+
+	class Ventana_listener implements WindowListener {
+		@Override
+		public void windowActivated(WindowEvent arg0) {
+			System.out.println("Ventana " + num_ventana + " activada");
+		}
+
+		@Override
+		public void windowClosed(WindowEvent arg0) {
+			System.out.println("La ventana " + num_ventana + " ha sido cerrada");
+		}
+
+		@Override
+		public void windowClosing(WindowEvent arg0) {
+			System.out.println("Cerrando la ventana "+ num_ventana);
+		}
+
+		@Override
+		public void windowDeactivated(WindowEvent arg0) {
+			System.out.println("Ventana " + num_ventana + " desactivada");
+		}
+
+		@Override
+		public void windowDeiconified(WindowEvent arg0) {
+			System.out.println("Ventana " + num_ventana + " maximizada");
+		}
+
+		@Override
+		public void windowIconified(WindowEvent arg0) {
+			System.out.println("Ventana " + num_ventana + " minimizada");
+		}
+
+		@Override
+		public void windowOpened(WindowEvent arg0) {
+			System.out.println("Se abrió la ventana "+ num_ventana);
+		}
+	}
+}
