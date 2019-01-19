e052c6dde3094a426bc73e81f7eb53a77255fd10
Agustín Achille <agustin.a.fr@gmail.com>
2019-01-18 21:12:05 -0300

    ventanas, trabajando con windowStateListener para cambios de estado como maximizar

---
+++ Pildoras_informaticas/src/g_events_5_windowStateListener/Ventana.java
@@ -0,0 +1,49 @@
+package g_events_5_windowStateListener;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class Ventana extends JFrame {
+	private static int sig_num_ventana = 1;
+	private int num_ventana;
+
+	public Ventana() {
+		num_ventana = sig_num_ventana;
+		sig_num_ventana++;
+		setTitle("Ventana " + num_ventana);
+		setSize(400, 300);
+		setLocation(100 + 50 * num_ventana, 100 + 50 * num_ventana);
+		setVisible(true);
+
+		addWindowStateListener(new Ventana_listener());
+	}
+
+	class Ventana_listener implements WindowStateListener {
+
+		@Override
+		public void windowStateChanged(WindowEvent e) {
+
+			switch (e.getNewState()) {
+				case Frame.NORMAL:
+					System.out.println("La ventana " + num_ventana +" se encuentra en estado normal");
+					break;
+				case Cursor.NE_RESIZE_CURSOR:
+					System.out.println("La ventana " + num_ventana +" se minimizó estando maximizada");
+					break;
+				case Frame.ICONIFIED:
+					System.out.println("La ventana " + num_ventana +" se minimizó");
+					break;
+			
+				case Frame.MAXIMIZED_BOTH:
+					System.out.println("La ventana " + num_ventana +" maximizó");
+					break;
+				default:
+					System.out.println(e.getNewState());
+
+			}
+
+		}
+	}
+}
