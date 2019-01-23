262e21e746d2b4271fcdd9338c59e15de6f42a40
Agustín Achille <agustin.a.fr@gmail.com>
2019-01-23 19:46:32 -0300

    text editor 2.0

--- Pildoras_proyectos_procesador_de_texto/src/procesador_de_texto/Capa_principal.java
+++ Pildoras_proyectos_procesador_de_texto/src/procesador_de_texto/Capa_principal.java
@@ -4,33 +4,31 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.event.*;
+import javax.swing.text.StyledEditorKit;
 
 public class Capa_principal extends JPanel{
-	private enum Font_prop{
-		FAMILY, STYLE, SIZE
-	}
-	
 	//Variables
 	private JPanel capa_menu = new JPanel();
 	
 	private JMenuBar barra_principal = new JMenuBar();
 	
 	private JMenu menu_fuente = new JMenu("Fuente");
-		private JMenuItem fuente_arial = new JMenuItem( new Change_font("Arial", Font_prop.FAMILY) );
-		private JMenuItem fuente_courier = new JMenuItem( new Change_font("Courier", Font_prop.FAMILY) );
-		private JMenuItem fuente_verdana = new JMenuItem( new Change_font("Verdana", Font_prop.FAMILY) );
+		private JMenuItem fuente_arial = new JMenuItem( new StyledEditorKit.FontFamilyAction("Arial", "Arial") );
+		private JMenuItem fuente_courier = new JMenuItem( new StyledEditorKit.FontFamilyAction("Courier", "Courier") );
+		private JMenuItem fuente_verdana = new JMenuItem( new StyledEditorKit.FontFamilyAction("Verdana", "Verdana") );
 	
 	private JMenu menu_estilo = new JMenu("Estilo");
-		private JMenuItem estilo_bold = new JMenuItem( new Change_font("Bold", Font.BOLD, Font_prop.STYLE) );
-		private JMenuItem estilo_italic = new JMenuItem( new Change_font("Italic", Font.ITALIC, Font_prop.STYLE) );
+		private JMenuItem estilo_bold = new JMenuItem( new StyledEditorKit.BoldAction());
+		private JMenuItem estilo_italic = new JMenuItem( new StyledEditorKit.ItalicAction() );
 	
 	private JMenu menu_tamano = new JMenu("Tamaño");
-		private JMenuItem tamano_12 = new JMenuItem( new Change_font("12", Font_prop.SIZE));
-		private JMenuItem tamano_14 = new JMenuItem( new Change_font("14", Font_prop.SIZE));
-		private JMenuItem tamano_16 = new JMenuItem( new Change_font("18", Font_prop.SIZE));
-		private JMenuItem tamano_18 = new JMenuItem( new Change_font("20", Font_prop.SIZE));
+		private JMenuItem tamano_12 = new JMenuItem( new StyledEditorKit.FontSizeAction("12", 12));
+		private JMenuItem tamano_14 = new JMenuItem( new StyledEditorKit.FontSizeAction("16", 16));
+		private JMenuItem tamano_16 = new JMenuItem( new StyledEditorKit.FontSizeAction("18", 18));
+		private JMenuItem tamano_18 = new JMenuItem( new StyledEditorKit.FontSizeAction("20", 20));
 	
 	private JTextPane pagina = new JTextPane();
+	
 	//--------------------------------------------------------------------------------------------------
 	
 	//Constructor
@@ -62,41 +60,5 @@ public class Capa_principal extends JPanel{
 	}
 	
 	//Eventos
-	private class Change_font extends AbstractAction {
-		Font_prop category;
-		int add_style;
-		
-		public Change_font(String font_name, Font_prop category) {
-			this.category = category;
-			putValue(NAME, font_name);
-		}
-		
-		public Change_font(String style_name, int style, Font_prop category) {
-			add_style = style;
-			this.category = category;
-			putValue(NAME, style_name);
-		}
-		
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			Font font = pagina.getFont();
-			String font_name = font.getFamily();
-			int style = font.getStyle();
-			int size = font.getSize();
-			
-			switch(category) {
-				case FAMILY:
-					pagina.setFont(new Font(getValue(NAME).toString(), style, size));		
-					break;
-					
-				case STYLE:
-					pagina.setFont(new Font(font_name, style^add_style, size));			
-					break;
-					
-				case SIZE:
-					size = Integer.parseInt(getValue(NAME).toString());
-					pagina.setFont(new Font(font_name, style, size));			
-			}			
-		}	
-	}
+	
 }
